e37a34096e27aaca2fb8840a7c3b7589da964d86
FareedKandalaft <fareed@spectrecoder.com>
2021-04-22 11:47:13 -0400

    use state and props. use window.navigator.geolocation.getCurrentPosition

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SeasonDisplay = (props) => {
+  return <div>Season Display - Lat: {props.lat}</div>;
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-  return <div>Hi There!</div>;
-};
+class App extends Component {
+  state = { lat: null, errMsg: '' };
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errMsg: err.message })
+    );
+  }
+
+  render() {
+    if (this.state.errMsg && !this.state.lat) {
+      return <div>Error: {this.state.errMsg}</div>;
+    }
+
+    if (!this.state.errMsg && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <div>Loading!</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
